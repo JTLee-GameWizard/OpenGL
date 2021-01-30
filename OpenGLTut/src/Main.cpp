c7160353f16f70eb1f9baa049c83a2fc5a0af83b
GitRangerNumberOne <wujoshuagamedev@gmail.com>
2021-01-30 13:29:55 -0600

    added a window

--- OpenGLTut/src/Main.cpp
+++ OpenGLTut/src/Main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+const int windowWidth = 1280;
+const int windowHeight = 720;
 int main()
 {
 	glfwInit();
-	gladLoadGLLoader(GLADloadproc(glfwGetProcAddress));
 	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "OpenGLRenderWindow", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+	
+	gladLoadGLLoader(GLADloadproc(glfwGetProcAddress));
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.5, 0.3, 0.1, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 	std::cout << "hello world!\n";
 }
