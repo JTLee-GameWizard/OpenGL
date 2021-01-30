a23d9f5f8ff80230a2027dfa67e395570cdaa3d8
GitRangerNumberOne <wujoshuagamedev@gmail.com>
2021-01-30 13:46:58 -0600

    added triangle

--- OpenGLTut/src/Main.cpp
+++ OpenGLTut/src/Main.cpp
@@ -16,10 +16,29 @@ int main()
 	
 	gladLoadGLLoader(GLADloadproc(glfwGetProcAddress));
 
+	float vertices[] = {
+		-0.5f, -0.5f, 0.f,
+		0.5f,  -0.5f, 0.f,
+		0.f,    0.5f,  0.f
+	};
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+	glEnableVertexAttribArray(0);
+	
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.5, 0.3, 0.1, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
